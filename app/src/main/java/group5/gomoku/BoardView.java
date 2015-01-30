e9340e75d704947306e6539f7b0aefd6ddc76aa6
Poolet <tpoole@pdx.edu>
2015-01-29 16:53:40 -0800

    Redraw grid
    I redid the way I am drawing the grid for now. Button clicks don't place
    tiles anymore, but I think I am going to need to do it this way to make
    it work if we want to drop pieces on the grid lines instead of in the
    tiles

---
+++ app/src/main/java/group5/gomoku/BoardView.java
@@ -0,0 +1,98 @@
+package group5.gomoku;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Tyler on 1/29/2015.
+ */
+public class BoardView extends View {
+    private int gridDimension;
+
+    private Paint gridPaint;
+    private Paint axisPaint;
+
+    public BoardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        Init();
+    }
+
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Init();
+    }
+
+    public BoardView(Context context) {
+        super(context);
+        Init();
+    }
+
+    protected void onDraw(Canvas canvas) {
+        // Parent draw
+        super.onDraw(canvas);
+
+        // Background color
+        canvas.drawColor(Color.TRANSPARENT);
+
+        // Draw vertical lines
+        for (int x = -this.getGridDimension(); x <= this.getGridDimension(); ++x)
+            canvas.drawLine(interpX(x), interpY(this.getGridDimension()), interpX(x), interpY(-this.getGridDimension()), gridPaint);
+
+        // Draw horizontal lines
+        for (int y = -this.getGridDimension(); y <= this.getGridDimension(); ++y)
+            canvas.drawLine(interpX(-this.getGridDimension()), interpY(y), interpX(this.getGridDimension()), interpY(y), gridPaint);
+    }
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredHeight = measureHeight(heightMeasureSpec);
+        int measuredWidth = measureWidth(widthMeasureSpec);
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    private int measureWidth(int widthMeasureSpec) {
+        int specSize = MeasureSpec.getSize(widthMeasureSpec) -
+                this.getPaddingLeft() - this.getPaddingRight();
+
+        return specSize;
+    }
+    private int measureHeight(int heightMeasureSpec) {
+        int specSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        return specSize;
+    }
+    public int getGridDimension() {
+        return gridDimension;
+    }
+
+    public void setGridDimension(int gridDimension) {
+        this.gridDimension = gridDimension;
+    }
+
+    public void Init() {
+        // Set initial grid dimension
+        setGridDimension(5);
+
+        // Grid line paint
+        gridPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        gridPaint.setStyle(Paint.Style.STROKE);
+        gridPaint.setStrokeWidth(3);
+        gridPaint.setColor(Color.BLACK);
+    }
+
+    private float interpX(double x) {
+        double width = (double) this.getWidth();
+        return (float) ((x + this.getGridDimension())
+                / (this.getGridDimension() * 2) * width);
+    }
+
+    private float interpY(double y) {
+        double height = (double) this.getHeight();
+        return (float) ((y + this.getGridDimension())
+                / (this.getGridDimension() * 2) * -height + height);
+    }
+}
